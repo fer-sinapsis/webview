75f318a56493d6670eda6ac09b1c450d9a77a035
fernando <fernando.haring@sinapsis.co>
2023-03-30 16:28:46 -0300

    fix width

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import styled from '@mui/material/styles/styled';
-import Box from '@mui/material/Box';
-import { Button, Typography } from '@mui/material';
+import React from "react";
+import styled from "@mui/material/styles/styled";
+import Box from "@mui/material/Box";
+import { Button, Typography } from "@mui/material";
 
 const Container = styled(Box)(() => {
   return {
     height: "100vh",
-    width: "100vw",
+    width: "100%",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
@@ -18,40 +18,36 @@ const NewButton = styled(Button)(() => {
   return {
     backgroundColor: "red",
     color: "white",
-    boxShadow: 'unset',
+    boxShadow: "unset",
     "&:hover": {
       backgroundColor: "blue",
     },
   };
 });
 
-
 const HomePage = () => {
-
-  
   function grantCameraPermission() {
     const event = {
-      type: 'camera_permission',
+      type: "camera_permission",
       hasPermission: true,
     };
     sendMessageToFlutter(event);
   }
-  
+
   function sendMessageToFlutter(message: any) {
     window.ReactNativeWebView.postMessage(JSON.stringify(message));
   }
-  
+
   return (
     <Container>
-        <Typography variant="h1" component="h1">
-          Press to open Camera
-        </Typography>
+      <Typography variant="h1" component="h1">
+        Press to open Camera
+      </Typography>
       <NewButton onClick={grantCameraPermission}>
         Grant Camera Permission
       </NewButton>
-      
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
